a5c493d04c392ee2197039953d2df225dc6ad4f8
divyasonara16 <76935234+divyasonara16@users.noreply.github.com>
2021-10-01 12:08:15 +0530

    Update main.go

--- main.go
+++ main.go
@@ -9,15 +9,19 @@ import (
 	"net/http"
 	"text/template"
 
+	"github.com/gorilla/mux"
 	flipt "github.com/markphelps/flipt-grpc-go"
 
 	"google.golang.org/grpc"
 )
 
-type data struct {
+type FetureFlag struct {
 	FlagKey     string
 	FlagName    string
 	FlagEnabled bool
+	Plan        string
+	ErrorMsg    string
+	IsError     bool
 }
 
 var (
@@ -27,10 +31,32 @@ var (
 
 func init() {
 	flag.StringVar(&fliptServer, "server", ":9000", "address of Flipt backend server")
-	flag.StringVar(&flagKey, "color-example", "color-example", "flag key to query")
+	flag.StringVar(&flagKey, "plan-rollout", "plan-rollout", "flag key to query")
 }
 
 func main() {
+	log.Println("demo ui available at http://localhost:8000")
+	log.Printf("flag key: %s\n", flagKey)
+	myRouter := mux.NewRouter().StrictSlash(true)
+	myRouter.HandleFunc("/", Default).Methods("GET")
+	myRouter.HandleFunc("/plan", Plan).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8000", myRouter))
+
+}
+
+func Default(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("plan.html"))
+	data := FetureFlag{
+		IsError:  true,
+		ErrorMsg: "Enter http://localhost:8000/plan?name=YourPlanName",
+	}
+	if err := t.Execute(w, data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
+
+func Plan(w http.ResponseWriter, r *http.Request) {
 	flag.Parse()
 
 	conn, err := grpc.Dial(fliptServer, grpc.WithInsecure())
@@ -42,38 +68,47 @@ func main() {
 	log.Printf("connected to Flipt server at: %s", fliptServer)
 
 	client := flipt.NewFliptClient(conn)
+	params := r.URL.Query().Get("name")
+	t := template.Must(template.ParseFiles("plan.html"))
 
-	t := template.Must(template.ParseFiles("basic.html"))
-
-	h := http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		flag, err := client.GetFlag(context.Background(), &flipt.GetFlagRequest{
-			Key: flagKey,
-		})
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		if flag == nil {
-			http.Error(w, "flag not found", http.StatusNotFound)
-			return
-		}
-
-		log.Printf("got flag: %v", flag)
-
-		data := data{
-			FlagKey:     flag.Key,
-			FlagName:    flag.Name,
-			FlagEnabled: flag.Enabled,
-		}
-
-		if err := t.Execute(w, data); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
+	var data FetureFlag
+	plan := params
+	flagKey := "plan-rollout"
+	entityId := "18568591hhuyuyuyyyyyyftgfsesxfc"
+	flag, err := client.Evaluate(context.Background(), &flipt.EvaluationRequest{
+		FlagKey:  flagKey,
+		EntityId: entityId,
+		Context: map[string]string{
+			"plan": plan,
+		},
 	})
 
-	log.Println("demo ui available at http://localhost:8000")
-	log.Printf("flag key: %s\n", flagKey)
-	log.Fatal(http.ListenAndServe(":8000", h))
+	if flag.GetMatch() == true && flag.GetSegmentKey() == plan {
+		data = FetureFlag{
+			FlagKey:     flagKey,
+			FlagName:    flagKey,
+			FlagEnabled: flag.GetMatch(),
+			Plan:        plan,
+		}
+	} else {
+		data = FetureFlag{
+			IsError:  true,
+			ErrorMsg: "You need to register plan",
+		}
+	}
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	if flag == nil {
+		http.Error(w, "flag not found", http.StatusNotFound)
+		return
+	}
+
+	if err := t.Execute(w, data); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
 }
